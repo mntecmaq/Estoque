1570cf6d06d7ccc2286150ed85e3f84b2d36b3ed
mntecmaq <mntecmaq@gmail.com>
2026-03-17 17:04:02 -0300

    Restore full menu options in sidebar

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ st.title("🛠️ Sistema de Gestão de estoque (Nuvem)")
 #menu = ["estoque Atual", "Cadastrar Fornecedor", "Entrada (Compra)", "Saída (Uso/Venda)"]
 #choice = st.sidebar.selectbox("Menu de Navegação", menu)
 # Adicione após a linha 18:
-menu = ["estoque Atual"]
+menu = ["estoque Atual", "Cadastrar Fornecedor", "Entrada (Compra)", "Saída (Uso/Venda)"]
 choice = st.sidebar.selectbox("Menu de Navegação", menu)
 
 # Fechar/esconder o sidebar após seleção
