b1928071d8a921be3708debb2a5aaee7287a0e8f
mntecmaq <mntecmaq@gmail.com>
2026-03-20 19:20:04 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ elif choice == "Cadastrar Fornecedor":
     
     # Criamos um formulário para encapsular os campos
     with st.form("form_fornecedor", clear_on_submit=True):
-        nome = st.text_input("Nome da Empresa/Vendedor")
+        nome_f = st.text_input("Nome da Empresa/Vendedor")
         contato = st.text_input("Telefone ou E-mail")
         endereco = st.text_input("Endereço")
         
@@ -70,7 +70,7 @@ elif choice == "Cadastrar Fornecedor":
         
         # A lógica só roda se o botão for pressionado
         if submit_button:
-            if nome:  # Verifica se o nome não está vazio
+            if nome_f:  # Verifica se o nome não está vazio
                 supabase.table("fornecedor").insert({"nome": nome_f, "contato": fone_f, "endereco": end_f}).execute()
                 st.success(f"Fornecedor {nome_f} cadastrado com sucesso!")
             else:
