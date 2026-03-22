43e8f315a1939bc43717bf3ca15b02d6f1bcb815
mntecmaq <mntecmaq@gmail.com>
2026-03-22 13:56:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -99,16 +99,16 @@ elif choice == "Entrada (Compra)":
 
             if res.data:
                 nova_qtd = res.data[0]['qtd'] + qtd_entrada
-                supabase.table("produtos").update({"qtd": nova_qtd}).eq("nome", produto_nome).execute()
+                supabase.table("produtos").update({"qtd_prd": nova_qtd}).eq("nome", produto_nome).execute()
             else:
-                supabase.table("produtos").insert({"nome": produto_nome, "qtd": qtd_entrada, "estoque_min": est_min}).execute()
+                supabase.table("produtos").insert({"nome": produto_nome, "qtd_prd": qtd_entrada, "estoque_min": est_min}).execute()
 
             # Regista histórico
             supabase.table("movimentacoes").insert({
                 "data": datetime.now().isoformat(),
                 "tipo": "ENTRADA",
                 "produto": produto_nome,
-                "qtd": qtd_entrada,
+                "qtd_prd": qtd_entrada,
                 "origem_destino": forn_choice
             }).execute()
             st.success("estoque atualizado com sucesso!")
