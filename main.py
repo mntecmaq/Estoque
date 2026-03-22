a46391ac6cf760efc886c725b8257a92dbad56d3
mntecmaq <mntecmaq@gmail.com>
2026-03-22 15:23:34 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ elif choice == "Entrada (Compra)":
          #   res = supabase.table("produtos").select("*").eq("produto", produto_nome).execute()
 
            # if res.data:
-                nova_qtd = res.data[0]['qnt_prd'] + qtd_entrada
+              #  nova_qtd = res.data[0]['qnt_prd'] + qtd_entrada
                 supabase.table("produtos").update({"qnt_prd": nova_qtd}).eq("produto", produto_nome).execute()
             else:
                 supabase.table("produtos").insert({"produto": produto_nome, "qnt_prd": qtd_entrada, "estmin": estmin}).execute()
